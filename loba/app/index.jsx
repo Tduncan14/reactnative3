864b029deb71d44bc608d11ccf4392c8da0763e7
tduncan14 <nbatre1@yahoo.com>
2025-03-23 02:59:04 -0500

    keep moving forward

---
+++ loba/app/index.jsx
@@ -0,0 +1,26 @@
+import { Text, View, StyleSheet } from "react-native";
+
+
+const HomeScreen = () => {
+
+
+
+
+
+  return (
+    <View>
+      <Text>Edit</Text>
+    </View>
+  )
+}
+
+
+const Styles = StyleSheet.create({
+
+
+
+
+})
+
+
+export default HomeScreen
