d50fedfc91c2a459b65ea96a0cf2d8703c635396
tduncan14 <nbatre1@yahoo.com>
2025-03-24 02:05:48 -0500

    keep moving forward

--- loba/app/index.jsx
+++ loba/app/index.jsx
@@ -1,15 +1,29 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import PostImage from '@/assets/images/images/post-it.png';
+import { useRouter } from "expo-router";
 
 
 const HomeScreen = () => {
 
 
+  const router = useRouter();
+
 
 
 
   return (
-    <View>
-      <Text>Edit</Text>
+    <View style={Styles.container}>
+      <Image source={PostImage} style={Styles.image} />
+      <Text style={Styles.title}>Welcome To Notes App</Text>
+      <Text style={Styles.subTitle}>Capture your Thoughts</Text>
+
+      <TouchableOpacity
+        style={Styles.button}
+        onPress={() => router.push('/notes')}>
+
+        <Text style={Styles.buttonText}> Get Started</Text>
+
+      </TouchableOpacity>
     </View>
   )
 }
@@ -18,6 +32,49 @@ const HomeScreen = () => {
 const Styles = StyleSheet.create({
 
 
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f8f9fa'
+
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+    borderRadius: 10
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333'
+
+  },
+
+  subTitle: {
+    fontSize: 16,
+    color: '#655',
+    textAlign: 'center',
+    marginBottom: 20
+  },
+
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
 
 
 })
