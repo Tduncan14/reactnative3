5dfb0056114b8d644d916485299025361538e643
tduncan14 <nbatre1@yahoo.com>
2025-05-01 20:28:32 -0500

    keep moving forward

--- loba/services/noteService.js
+++ loba/services/noteService.js
@@ -1,19 +1,27 @@
 import databaseService from "./databaseService";
 import { ID } from "react-native-appwrite";
 
+
+//Appwrite database and collection id 
+
+
 const dbId = process.env.EXPO_PUBLIC_APPWRITE_DB_ID;
 const colId = process.env.EXPO_PUBLIC_APPWRITE_COL_NOTES_ID;
 
 const noteService = {
+    // get Notes
+
     async getNotes() {
-        const response = await databaseService.ListDocuments(dbId, colId);
+        const response = await databaseService.ListDocuments(dbId, colID);
 
         if (response.error) {
             return { error: response.error };
         }
 
-        return { data: response.data }; // Return the actual documents
-    },
-};
+        return { data: response };
 
-export default noteService;
+    }
+}
+
+
+export default noteService
